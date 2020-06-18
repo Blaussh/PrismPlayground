a7d4648c4644cb892d4cc93dcb9f4d9fc25ccdc2
Shai Blaustien <shai.gm@gmail.com>
2020-06-18 20:08:05 +0300

    Added navigation to mail list

--- PrismOutlook/Modules/PrismOutlook.Modules.Mail/ViewModels/MailGroupViewModel.cs
+++ PrismOutlook/Modules/PrismOutlook.Modules.Mail/ViewModels/MailGroupViewModel.cs
@@ -1,5 +1,7 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using PrismOutlook.Buisness;
+using PrismOutlook.Core;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -18,19 +20,34 @@ namespace PrismOutlook.Modules.Mail.ViewModels
             set { SetProperty(ref _items, value); }
         }
 
-        public MailGroupViewModel()
+        private DelegateCommand<NavigationItem> _selectedCommand;
+        private readonly IApplicationCommands _applicationCommands;
+
+        public DelegateCommand<NavigationItem> SelectedCommand =>
+            _selectedCommand ?? (_selectedCommand = new DelegateCommand<NavigationItem>(ExecuteCommandName));
+
+        public MailGroupViewModel(IApplicationCommands applicationCommands)
         {
             GenerateMenu();
+            _applicationCommands = applicationCommands;
+        }
+
+        void ExecuteCommandName(NavigationItem navigationItem)
+        {
+            if (navigationItem != null)
+            {
+                _applicationCommands.NavigateCommand.Execute(navigationItem.NavigationPath);
+            }
         }
 
         void GenerateMenu()
         {
             Items = new ObservableCollection<NavigationItem>();
 
-            var root = new NavigationItem() { Caption = "Personal Folder", NavigationPath = "MailList" };
-            root.Items.Add(new NavigationItem() { Caption = "Inbox", NavigationPath = "" });
-            root.Items.Add(new NavigationItem() { Caption = "Deleted", NavigationPath = "" });
-            root.Items.Add(new NavigationItem() { Caption = "Sent", NavigationPath = "" });
+            var root = new NavigationItem() { Caption = "Personal Folder", NavigationPath = "MailList?id=Default" };
+            root.Items.Add(new NavigationItem() { Caption = "Inbox", NavigationPath = "MailList?id=Inbox" });
+            root.Items.Add(new NavigationItem() { Caption = "Deleted", NavigationPath = "MailList?id=Delete" });
+            root.Items.Add(new NavigationItem() { Caption = "Sent", NavigationPath = "MailList?id=Sent" });
 
             Items.Add(root);
         }
